87732e969dc6a05a5ba48fba5fe5e50fe730369c
Alberto Damo <alberto.damo@gmail.com>
2024-07-09 18:06:30 +0200

    thinking on how to implement

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+mod combinator{
+    #[derive(Debug,PartialEq)]
+    pub struct Combinator{
+        prim_port: Box<Option<Combinator>>,
+        aux_port: Option<Vec<Option<Box<Combinator>>>>,
+    }
+
+    impl Combinator {
+        pub fn new(num_aux_port: u8) ->Combinator {
+            
+            let mut aux_port_maybe = None;
+            if num_aux_port > 0{
+                let mut aux_port : Vec<Option<Box<Combinator>>> = Vec::new();
+                for _ in 0..num_aux_port{
+                    aux_port.push(None);
+                }
+                aux_port_maybe = Some(aux_port);
+            }
+
+            Combinator{
+                prim_port: Box::new(None),
+                aux_port: aux_port_maybe,
+            }
+        }
+        
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let comb = combinator::Combinator::new(0);
 }
