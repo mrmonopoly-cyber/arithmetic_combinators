6451ff974185c464130dc8456cb5f36b95df2e70
Alberto Damo <alberto.damo@gmail.com>
2024-07-19 22:56:11 +0200

    cleaning

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,11 @@ fn main() {
     let graph = Box::leak(Box::new(arith_combinator_graph::new_graph()));
     let zero = arith_combinator_graph::create_op(ArithOp::ZERO);
     let inc = arith_combinator_graph::create_op(ArithOp::INC);
-    let pos = arith_combinator_graph::create_op(ArithOp::POS);
     let sum = arith_combinator_graph::create_op(ArithOp::SUM);
 
     graph.attach(zero.label);
     graph.attach(inc.label);
     graph.attach(sum.label);
-    // graph.attach(inc.label);
     graph.attach(zero.label);
 
     graph.print_graph();
